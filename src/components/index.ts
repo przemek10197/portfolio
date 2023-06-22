aefcc576906e6c6505b33bc3f528dac5eecf8b49
Przemyslaw Tazbirek <przemyslaw.tazbirek@billennium.com>
2023-06-22 09:32:36 +0200

    add components global import

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+import DsCard from './dsCard/DsCard.vue';
+import DsIcon from './dsIcon/DsIcon.vue';
+import DsMainButton from './dsMainButton/DsMainButton.vue';
+import DsSecondaryButton from './dsSecondaryButton/DsSecondaryButton.vue';
+
+export { DsCard, DsIcon, DsMainButton, DsSecondaryButton };
